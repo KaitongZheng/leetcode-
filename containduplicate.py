4caaa9e2bb0905f2e4a33462a79d6b0e326e44ca
zkt111 <39017065+zkt111@users.noreply.github.com>
2019-08-04 14:10:47 +0800

    Update containduplicate.py

--- containduplicate.py
+++ containduplicate.py
@@ -14,3 +14,15 @@ b= Solution()
 c=b.containsDuplicate(a)
 print(a)
 print(c)
+
+'''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for n in nums:
+            if n in dic: return True
+            dic[n] = 1
+        return False
+查找list中是否存在duplicate
+查找的例子都可以使用hash表降低时间复杂度
+'''
